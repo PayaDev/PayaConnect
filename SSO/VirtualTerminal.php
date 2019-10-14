d2ff22416c228ef6beb3ea42087749b74b0d82a8
Thomas Hagan <thlinux1107@users.noreply.github.com>
2019-10-14 12:18:16 -0400

    Adding SSO Virtual Terminal Sample
    Created:
    VirtualTerminal.php
    -Builds SSO link for processing a transaction through the Virtual Terminal UI.
    
    shared.php
    -Contains the shared credentials for the sample

---
+++ SSO/VirtualTerminal.php
@@ -0,0 +1,77 @@
+<?php
+
+/*----------------------------------------------
+Author: SDK Support Group
+Company: Paya
+Contact: sdksupport@paya.com
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!! Samples intended for educational use only!!!
+!!!        Not intended for production       !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-----------------------------------------------*/    
+
+    require('shared.php');
+    
+    // Location Variables
+    $locationID = $location['ID'];
+    
+    // Developer Variables
+    $developerID = $developer['ID'];
+    $host = $developer['Host'];
+    
+    // User Variables
+    $userID = $user['ID'];
+    $userAPIKey = $user['APIKey'];
+    
+    // Transaction Variables
+    $verb = "GET";
+    $endpoint = "/custom/contactsso?";
+    $timestamp = time();
+    
+    // Build the request
+    $req = [
+        "timestamp" => $timestamp,
+        "user_id" => $userID,
+        "user_api_key" => $userAPIKey,
+        "location_id" => $locationID,
+        "route" => "virtualterminal",
+        "params" =>	[
+          "transaction_amount" => "1.00",
+          "billing_street" => "123 Main Street",
+          "billing_zip" => "31405",
+          "order_num" => "SDK Test Invoice " . $timestamp
+        ]
+    ];
+    
+    $payload = bin2hex(json_encode($req));
+    $url = $host . $endpoint . 'developer-id=' . $developerID . '&data=' . $payload;
+
+
+?>
+
+<!--Create a page to display the payment link.-->
+<style>
+    a {
+        display:inline-block;
+        background-color:#428bca;
+        border-color:#357ebd;
+        border-radius:5px;
+        border-width:0;
+        border-style:none;
+        color:#ffffff;
+        font-size:12px;
+        height:30px;
+        width:100px;
+        margin:0px;
+        padding:7px;
+        text-decoration:none;
+        text-align:center;
+    }
+</style>
+
+<div>
+    <h1>Paya Connect SSO Virtual Terminal</h1>
+    <br />
+    <!--Build the payment link.-->
+    <a href="<?= $url ?>" type="button">Go To Payment Form</a>
+</div>
