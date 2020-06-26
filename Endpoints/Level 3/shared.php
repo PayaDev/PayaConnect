58a5ed7588be8f5a171245be55d03439b9c96c11
Thomas Hagan <thlinux1107@users.noreply.github.com>
2020-06-26 12:24:30 -0400

    Added Level 3 Sample
    Uses the Transaction ID from a previous Sale or Authorization to add Level 3 data to the payment request.

---
+++ Endpoints/Level 3/shared.php
@@ -0,0 +1,46 @@
+<?php
+/*----------------------------------------------
+Author: SDK Support GroupCompany: Paya
+Contact: sdksupport@paya.com
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!! Samples intended for educational use only!!!
+!!!        Not intended for production       !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-----------------------------------------------*/    
+    
+    // Location Credentials
+	// This is provided by the merchant. A test location has been created.
+	// You can find your test Location ID under Project Details on the developer portal.
+    $location = [
+        "ID" => "[Location ID]",
+    ];
+    
+    // User Credentials
+	// The integrator may provide a single user account for use with the API. This
+	// same user account may then be added to merchants' locations. Otherwise the
+	// merchant will need to provide a set of user credentials for use with the API.
+	// You can find your test user credentials under API Credentials on the developer portal.
+    $user = [
+        "ID" => "[User ID]",
+        "APIKey" => "User API Key",
+    ];
+    
+    // Developer Credentials
+	// Your test developer credentials are found under Project Details and API Credentials.
+    $developer = [
+        "ID" => "[Developer ID]",
+        "Host" => "https://api.sandbox.payaconnect.com"
+    ];
+    
+    // Function to write debug information to console.
+    function debug_to_console( $data ) {
+        $output = $data;
+        if ( is_array( $output ) )
+            $output = implode( ',', $output);
+    
+        echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+    }
+    
+
+    
+?>
---
+++ Endpoints/Level 3/submit_lvl3.php
@@ -0,0 +1,68 @@
+<?php
+
+/*----------------------------------------------
+Author: SDK Support Group
+Company: Paya
+Contact: sdksupport@paya.com
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!! Samples intended for educational use only!!!
+!!!        Not intended for production       !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-----------------------------------------------*/    
+
+    require('shared.php');
+    
+    // Developer Variables
+    $developerID = $developer['ID'];
+    $host = $developer['Host'];
+    
+    // User Variables
+    $userID = $user['ID'];
+    $userAPIKey = $user['APIKey'];
+    
+    // Endpoint Variables
+    $verb = "POST";
+	  $endpoint = "/v2/transactionlevel3s";
+    
+    // Build the request
+    $req = file_get_contents("request_lvl3.json");
+    $transactionID = "[Transaction ID]";
+    $req = str_replace('@transactionID', $transactionID, $req);
+    echo $req;
+    
+    //$payload = json_encode($req);
+    
+    // file_get_contents to submit the POST. cURL may be used as well.
+    $url = $host . $endpoint;
+    $config = [
+    "http" => [
+        "header" => [
+            "developer-id: " . $developerID,
+            "user-api-key: " . $userAPIKey,
+            "user-id: " . $userID,
+            "content-type: application/json",
+        ],
+        "method" => $verb,
+        "content" => $req,
+        "ignore_errors" => true // exposes response body on 4XX errors
+      ]
+    ];
+
+    $context = stream_context_create($config);
+    $result = file_get_contents($url, false, $context);
+    $response = json_decode($result);
+    $httpcode = http_response_code();
+    
+    // Pull response data for logs and display
+    $pretty_resp = json_encode($response, JSON_PRETTY_PRINT);
+    
+    echo '<pre>';
+    print('Response: ' . $pretty_resp . "\n");
+    echo '</pre>';
+    
+    debug_to_console("Response:");
+    debug_to_console($response);
+    debug_to_console("Error:");
+    debug_to_console($err);
+
+?>
