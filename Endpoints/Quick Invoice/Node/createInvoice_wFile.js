ba61e65afd75431ebf6a400c26f8d01cb327447e
David Gelhardt <david.gelhardt@gmail.com>
2023-11-15 16:42:04 -0500

    Update createInvoice_wFile.js

--- Endpoints/Quick Invoice/Node/createInvoice_wFile.js
+++ Endpoints/Quick Invoice/Node/createInvoice_wFile.js
@@ -1,7 +1,7 @@
 /*----------------------------------------------
 Author: SDK Support Group
 Company: Paya
-Contact: sdksupport@paya.com
+Contact: sdksupport@nuvei.com
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!! Samples intended for educational use only!!!
 !!!        Not intended for production       !!!
